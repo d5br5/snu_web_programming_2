0223c5d2c2e5a81cb82b610021e4215db68e2dd3
DohyungKim <ehgud456456@naver.com>
2020-11-19 13:36:54 +0900

    web auth

---
+++ class/1118/util.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto');
+
+const encryptPassword = (password) => crypto.createHash('sha512').update(password).digest('base64');
+
+module.exports={
+    encryptPassword
+};
